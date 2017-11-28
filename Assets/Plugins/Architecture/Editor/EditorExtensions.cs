323a7f8d6627e0f9c4d538738d3b87d759414985
Jurjen Biewenga <j.f.biewenga@gmail.com>
2017-11-28 14:14:26 +0100

    Added an item to the context menu of monobehaviours to automatically generate and fill the reference fields

---
+++ Assets/Plugins/Architecture/Editor/EditorExtensions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Architecture
+{
+	public static class EditorExtensions  {
+
+		[MenuItem("CONTEXT/MonoBehaviour/Generate empty reference fields", false)]
+		public static void FillEmptyReferences(MenuCommand menuCommand)
+		{
+			Object target = menuCommand.context;
+			SerializedObject serializedTarget = new SerializedObject(target);
+			SerializedProperty it = serializedTarget.GetIterator();
+			do
+			{
+				string type = Utils.GetPropertyType(it);
+				if (type.EndsWith("Reference"))
+				{
+					Component go = (it.serializedObject.targetObject as Component);
+					string path = "";
+					if (go != null)
+					{
+						if (go.gameObject.scene.IsValid())
+						{
+							path = string.Format(@"Scriptable Objects\{0}\{1}\{2}.asset", go.gameObject.scene.name, it.serializedObject.targetObject.name,
+							                     it.displayName);
+						}
+						else
+						{
+							path = string.Format(@"Scriptable Objects\{0}\{1}\{2}.asset", "Prefabs", it.serializedObject.targetObject.name,
+							                     it.displayName);
+						}
+
+						var useConstantProperty = it.FindPropertyRelative("UseConstant");
+						var variableProperty = it.FindPropertyRelative("Variable");
+
+						Utils.GenerateAndSetVariable(useConstantProperty, variableProperty, path);
+					}
+				}
+			} while (it.Next(true));
+		}
+	}
+}
