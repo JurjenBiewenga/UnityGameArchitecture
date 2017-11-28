b64dccdacb5c4f295acffd3da6bdd7ec80c517b6
Jurjen Biewenga <j.f.biewenga@gmail.com>
2017-11-28 12:53:26 +0100

    Fixed the defaultvalue and value being switched out

--- Assets/Plugins/Architecture/Editor/VariableDrawer.cs
+++ Assets/Plugins/Architecture/Editor/VariableDrawer.cs
@@ -6,28 +6,28 @@ using UnityEngine;
 
 namespace Architecture
 {
-	public class VariableDrawer : Editor
-	{
-		private SerializedProperty persistentProperty;
-		private SerializedProperty valueProperty;
-		private SerializedProperty defaultValueProperty;
-		
-		private void OnEnable()
-		{
-			persistentProperty = serializedObject.FindProperty("Persistent");
-			valueProperty = serializedObject.FindProperty("Value");
-			defaultValueProperty = serializedObject.FindProperty("DefaultValue");
-		}
+    public class VariableDrawer : Editor
+    {
+        private SerializedProperty persistentProperty;
+        private SerializedProperty valueProperty;
+        private SerializedProperty defaultValueProperty;
 
-		public override void OnInspectorGUI()
-		{
-			serializedObject.Update();
-			EditorGUILayout.PropertyField(persistentProperty);
-			if(persistentProperty.boolValue)
-				EditorGUILayout.PropertyField(defaultValueProperty);
-			else
-				EditorGUILayout.PropertyField(valueProperty);
-			serializedObject.ApplyModifiedProperties();
-		}
-	}
+        private void OnEnable()
+        {
+            persistentProperty = serializedObject.FindProperty("Persistent");
+            valueProperty = serializedObject.FindProperty("Value");
+            defaultValueProperty = serializedObject.FindProperty("DefaultValue");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(persistentProperty);
+            if (persistentProperty.boolValue)
+                EditorGUILayout.PropertyField(valueProperty);
+            else
+                EditorGUILayout.PropertyField(defaultValueProperty);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
 }
