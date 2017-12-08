cdde60150d2529b3fd7c5597d1c338ef586c77ff
Jurjen Biewenga <j.f.biewenga@gmail.com>
2017-12-08 12:04:17 +0100

    Added select architecture settings button to the tools menu

---
+++ Assets/Plugins/Architecture/Editor/MenuItems.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Architecture
+{
+    public static class MenuItems
+    {
+        [MenuItem("Tools/Select Architecture Settings")]
+        public static void SelectSettings()
+        {
+            ArchitectureSettings settings = ArchitectureSettings.GetSettings();
+            Selection.activeObject = settings;
+        }
+    }
+}
