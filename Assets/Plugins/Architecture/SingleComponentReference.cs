74e422495c81c97564fd8764ab579e1239f98c17
Jurjen Biewenga <j.f.biewenga@gmail.com>
2017-12-15 14:49:10 +0100

    Added check to see if the playmode will change in editor

--- Assets/Plugins/Architecture/SingleComponentReference.cs
+++ Assets/Plugins/Architecture/SingleComponentReference.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Architecture
 {
@@ -24,6 +27,20 @@ namespace Architecture
 					}
 				}
 			}
+#if UNITY_EDITOR
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+			{
+				if (FindOnEnable)
+				{
+					Value = FindObjectOfType<T>();
+					if (Value == null && CreateIfNotFound)
+					{
+						GameObject go = new GameObject(typeof(T).Name);
+						Value = go.AddComponent<T>();
+					}
+				}
+			}
+#endif
 		}
 	}
 }
